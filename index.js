26b4eed25206b0d602bf9b8defd6eed83ebe922f
Jacob McCracken <mccrackenjacob@gmail.com>
2020-07-18 21:25:41 +0000

    added es6 arrow function example and looked at an application of closures

--- index.js
+++ index.js
@@ -18,3 +18,16 @@ a()()()
 // grandpa and father remain in the memory heap because of the closure that prevents the garbage collector from removing it. They will remain active while they are referenced by the son ('child') function.
 
 // where we write the function matters. Not where we invoke the function.
+
+
+// example using es6 arrow functions. Able to write the whole thing on one line.
+
+const boo = (string) => (name) => (name2) =>
+      console.log(`${string} ${name} ${name2}`)
+
+boo('hi')('Tim')('Becca')
+
+// Provided the JavaScript engine is running we can hold onto information and call it whenever we want.
+const booString = boo('hi');
+// wait 5 years
+const booStringName = booString(); 
