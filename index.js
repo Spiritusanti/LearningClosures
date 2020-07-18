3cde5a2146a1c1b833a07ba9659cad228122fc07
Jacob McCracken <mccrackenjacob@gmail.com>
2020-07-18 21:01:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+// We have closures because of two things:
+// 1. functions are first class citizens in JavaScript
+// 2. Lexical scope, the engine knows where our code is written and what variables each function has access to.
+
